efa71b5915f7bcd338e285e194f1bd15d41b2e56
ManojNambiraj <postboxmano@gmail.com>
2025-04-16 19:43:39 +0530

    JS Class1

---
+++ JS/class1/script.js
@@ -0,0 +1,76 @@
+// alert("hi");
+// console.log("hi");
+
+// Javascript ---> Scripting Language
+
+    // EcmaScript
+
+    // JS Current Version -> ES6
+    // JS last version -> ES5
+
+
+// Variables
+
+    // abi_age_in_2005 = 5000
+
+    // abiagein2005;
+
+    // console.log(abi_age_in_2005);
+
+// Hoisting: (variable declaration)
+
+    // ES5:
+
+        // var a = 10
+
+        // var a = 500
+
+        // console.log(a)
+
+    // ES6:
+
+    // let
+
+        // let a
+
+        // a = 20
+
+        // console.log(a);
+
+    // const
+
+        // const a = 10
+
+        // console.log(a);
+
+// Scope:
+
+    // 1. Global Scope
+
+        // {
+        //     var a = 10
+
+        //     console.log("inside: ", a);
+            
+        // }
+
+        // console.log("Outside: ", a);
+        
+
+    // 2. Local Scope
+
+        // {
+        //     const a = 10;
+
+        //     console.log("inside: ", a);
+        // }
+
+        // console.log("Outside: ", a);
+
+// Datatypes:
+
+    let a = null
+
+    console.log(a, typeof(a))
+    
+
