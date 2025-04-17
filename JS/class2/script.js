17108b847730c370f58ebb12f8f542acd5a08086
ManojNambiraj <postboxmano@gmail.com>
2025-04-17 20:02:15 +0530

    JS Operators

---
+++ JS/class2/script.js
@@ -0,0 +1,95 @@
+// Operators:
+
+    // 1. Arithmetic Operators
+        // (+, -, *, /, %, **, ++, --)
+
+            // let a = 4
+            // let b = 2
+            // let result
+
+            // result = a + b;
+            // result = a - b;
+            // result = a * b;
+            // result = a / b;
+            // result = a % b;
+            // result = a ** b;
+            // result = a++
+            // result = --a
+
+            // console.log(result);
+
+    // 2. Assignment Operators
+         // (=, +=, -=, *=, /=, %=, **=)
+
+        //  let a = 5
+
+            //  a += 2  //  a = a + 2
+            //  a -= 2  //  a = a - 2
+            //  a *= 2  //  a = a * 2
+            //  a /= 2  //  a = a / 2
+            //  a %= 2  //  a = a % 2
+            //  a **= 2  //  a = a ** 2
+
+            //  console.log(a);
+         
+    // 3. Comparison Operators  --> (Boolean)
+        // (==, !=, ===, !==, <, >, <=, >=)
+
+            // let age = 16
+            // let result
+
+            // result = (age == 18)
+            // result = (age != 18)
+            // result = (age === 18)
+            // result = (age !== 18)
+            // result = (age < 18)
+            // result = (age > 18)
+            // result = (age <= 18)
+            // result = (age >= 18)
+
+            // console.log(result);
+
+    // 4. Logical Operators
+        // (&&, ||, !)
+
+        // &&
+
+            // (True)  && (True)  --> True
+            // (True)  && (False) --> False
+            // (False) && (True)  --> False
+            // (False) && (False) --> False
+
+        // ||
+
+            // (True)  || (True)  --> True
+            // (True)  || (False) --> True
+            // (False) || (True)  --> True
+            // (False) || (False) --> False
+
+        // !
+
+            // (True)  --> False
+            // (False) --> True
+
+        // let age = 17
+
+        // let result = (age == 18) && (age >= 18)
+        // let result = !((age == 18) || (age > 18))
+
+        // console.log(result);
+
+    // 5. Ternary Operators
+
+        // let age = 15
+
+        // let result = (age > 18) ? "Eligible for voting" : "Not eligible"
+
+        // console.log(result);
+        
+
+    // 6. Type Operators
+
+        // let a = 10
+
+        // console.log(a, typeof(a));
+    
