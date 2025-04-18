03fd75d4e9277e9d4e79c9c3c60b0be85d7bfe37
ManojNambiraj <postboxmano@gmail.com>
2025-04-18 19:45:43 +0530

    JS Control statements

---
+++ JS/class3/script.js
@@ -0,0 +1,96 @@
+// Control Statements:
+
+// 1. Conditional Stmts
+
+// if
+
+// let age = 15
+
+// if(age > 18){
+//     console.log("Eligible for voting");
+// }
+
+// if else
+
+// let age = 15;
+
+// if (age > 18) {
+//     console.log("Eligible for voting");
+// }else{
+//     console.log("Not eligible");
+// }
+
+// if else if
+
+// let age = 14;
+
+// if (age > 18) {
+//     console.log("Eligible for voting");
+// }else if(age == 18){
+//     console.log("Partially eligible")
+// }else {
+//     console.log("Not eligible");
+// }
+
+// switch case
+
+// let day = "Monday";
+
+// switch (day) {
+//   case "Monday":
+//     console.log("It's a Monday");
+//     break;
+//   case 2:
+//     console.log("It's a Tuesday");
+//     break;
+//   case 3:
+//     console.log("It's a Wednesday");
+//     break;
+//   case 4:
+//     console.log("It's a Thursday");
+//     break;
+//   case 5:
+//     console.log("It's a Friday");
+//     break;
+//   case 6:
+//     console.log("It's a Saturday");
+//     break;
+//   default:
+//     console.log("Happy Weekend");
+//     break;
+// }
+
+// 2. Looping stmts
+
+// for loop
+
+// for(stmt1; stmt2; stmt4){
+// -----stmt3------
+// }
+
+// stmt1 -> initializing
+// stmt2 -> conditions
+// stmt3 -> executable
+// stmt4 -> increment / decrement
+
+// for(let i = 5; i <= 20; i++){
+//     console.log(i);
+// }
+
+// while loop
+
+// let i = 1;
+
+// while(i <= 10){
+//     console.log(i);
+//     i++;
+// }
+
+// do...while loop
+
+// let i = 11;
+
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 10);
