85d317c514ec98fd6005a1f2e4f9f53a0bc73886
ManojNambiraj <postboxmano@gmail.com>
2025-04-21 19:42:46 +0530

    JS functions

--- JS/class3/script.js
+++ JS/class3/script.js
@@ -1,96 +1,116 @@
 // Control Statements:
 
-// 1. Conditional Stmts
+    // 1. Conditional Stmts
 
-// if
+        // if
 
-// let age = 15
+            // let age = 22
 
-// if(age > 18){
-//     console.log("Eligible for voting");
-// }
+            // if(age > 18){
+            //     console.log("Eligible for voting");
+            // }
+            
+        // if else
 
-// if else
+            // let age = 15;
 
-// let age = 15;
+            // if (age > 18) {
+            //     console.log("Eligible for voting");
+            // }else{
+            //     console.log("Not eligible");
+            // }
 
-// if (age > 18) {
-//     console.log("Eligible for voting");
-// }else{
-//     console.log("Not eligible");
-// }
+        // if else if
 
-// if else if
+            // let age = 18;
 
-// let age = 14;
+            // if (age > 18) {
+            //     console.log("Eligible for voting");
+            // }else if(age == 18){
+            //     console.log("Partially eligible")
+            // }else {
+            //     console.log("Not eligible");
+            // }
 
-// if (age > 18) {
-//     console.log("Eligible for voting");
-// }else if(age == 18){
-//     console.log("Partially eligible")
-// }else {
-//     console.log("Not eligible");
-// }
+        // switch case
 
-// switch case
+            // let day = "Monda";
 
-// let day = "Monday";
+            // switch (day) {
+            //   case "Monday":
+            //     console.log("It's a Monday");
+            //     break;
+            //   case 2:
+            //     console.log("It's a Tuesday");
+            //     break;
+            //   case 3:
+            //     console.log("It's a Wednesday");
+            //     break;
+            //   case 4:
+            //     console.log("It's a Thursday");
+            //     break;
+            //   case 5:
+            //     console.log("It's a Friday");
+            //     break;
+            //   case 6:
+            //     console.log("It's a Saturday");
+            //     break;
+            //   default:
+            //     console.log("Happy Weekend");
+            //     break;
+            // }
 
-// switch (day) {
-//   case "Monday":
-//     console.log("It's a Monday");
-//     break;
-//   case 2:
-//     console.log("It's a Tuesday");
-//     break;
-//   case 3:
-//     console.log("It's a Wednesday");
-//     break;
-//   case 4:
-//     console.log("It's a Thursday");
-//     break;
-//   case 5:
-//     console.log("It's a Friday");
-//     break;
-//   case 6:
-//     console.log("It's a Saturday");
-//     break;
-//   default:
-//     console.log("Happy Weekend");
-//     break;
-// }
+    // 2. Looping stmts
 
-// 2. Looping stmts
+        // for loop
 
-// for loop
+            // for(stmt1; stmt2; stmt4){
+                // -----stmt3------
+            // }
 
-// for(stmt1; stmt2; stmt4){
-// -----stmt3------
-// }
+            // stmt1 -> initializing
+            // stmt2 -> conditions
+            // stmt3 -> executable
+            // stmt4 -> increment / decrement
 
-// stmt1 -> initializing
-// stmt2 -> conditions
-// stmt3 -> executable
-// stmt4 -> increment / decrement
+            // for(let i = 1; i <= 10; i++){
+            //     console.log(i);
+            // }
 
-// for(let i = 5; i <= 20; i++){
-//     console.log(i);
-// }
+        // while loop
 
-// while loop
+            // let i = 11;
 
-// let i = 1;
+            // while(i <= 10){
+            //     console.log(i);
+            //     i++;
+            // }
 
-// while(i <= 10){
-//     console.log(i);
-//     i++;
-// }
+        // do...while loop
 
-// do...while loop
+            // let i = 11;
 
-// let i = 11;
+            // do {
+            //     console.log(i);
+            //     i++;
+            // } while (i <= 10);
 
-// do {
-//     console.log(i);
-//     i++;
-// } while (i <= 10);
+    // 3. Jumping Stmts:
+
+        // break
+
+            // for(let i = 0; i <= 5; i++){
+            //     if(i == 3){
+            //         break
+            //     }
+            //     console.log(i);
+            // }
+
+        // Continue
+
+            // for (let i = 0; i <= 10; i++) {
+            //   if (i % 2 != 0) {
+            //     continue;
+            //   }
+            //   console.log(i);
+            // }
---
+++ JS/class4/script.js
@@ -0,0 +1,63 @@
+// Functions:
+
+// 1. Normal Function:
+
+    // function demo(){     // function definition
+    //     for(let i = 0; i <= 5; i++){
+    //         console.log(i);
+    //     }
+    // }
+
+
+    // console.log("Good morning");
+
+    // demo()              // function call
+
+    // console.log("Good afternoon");
+
+    // demo()              // function call
+
+    // console.log("Good evening");
+
+        // Parameterized function
+
+        // function Mul(a, b, c){
+        //     return (a * b) + " " + c + b
+        // }
+
+        // let result1 = Mul(57, 2, "Multiplied by: ")
+
+        // console.log(result1);
+
+// 2. Ananymous function: (Unnamed function)
+
+    // const demo = function(){
+    //     console.log("Hi, I'm ananymous");
+    // }
+
+    // demo()
+
+// 3. Arrow function: (Single line function)
+
+    //  let arr = () => console.log("hi");
+
+    // let arr = (a) => a * 4;
+
+    // console.log(arr(5));
+ 
+// 4. IIFE (Immediately Invoked Function Expression)
+
+    // (() => {
+    //     console.log("IIFE");
+    // })()
+
+
+// Built-in functions:
+
+    // setTimeout(function() {
+    //   console.log("hi");
+    // }, 5000);
+
+    setInterval(() => {
+    console.log("hi");
+    }, 3000);
