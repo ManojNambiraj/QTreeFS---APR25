48752a4160ee32a1bc932de753eb9628b1ce7653
ManojNambiraj <postboxmano@gmail.com>
2025-04-22 20:24:52 +0530

    arrays and objects

---
+++ JS/class5/script.js
@@ -0,0 +1,127 @@
+// Arrays:
+
+        // let arr = [10, 20, 30, 40, 50, 60]
+
+        // console.log(arr);
+
+        // let arr1 = [1, "Hello", true]
+
+        // console.log(arr1);
+
+        // let fruits = ["apple", "orange", "banana", "kiwi", "dragon"];
+        // indexing     #0       #1         #2      #3
+
+        // console.log(fruits);
+        // console.log(fruits[2]);
+
+        // fruits[2] = "grapes"
+
+        // console.log(fruits.length);
+
+        // // fruits.pop()
+        // fruits.push("watermelon")
+
+        // console.log(fruits);
+
+        // for(let i = 0; i < fruits.length; i++){
+        //     console.log(fruits[i]);
+        // }
+
+    // ES6 -> Array Methods (MRF)
+
+    // M  -> map
+
+        // let arr = [10, 20, 30, 40, 50, 60, 70];
+
+        // let result = arr.map((item) => item * 2);
+
+        // console.log(result);
+
+    // F  -> Filter
+
+        // let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 8, 8, 8, 8]
+
+        // let result = arr.filter((item)=> item % 2 == 0)
+        // let result = arr.filter((item) => item > 8)
+
+        // console.log(result);
+        
+    // R  -> Reduce
+
+    //    let arr = [10, 20, 30, 40, 50, 60, 70];
+
+    //    let result = arr.reduce((acc, curr) => (acc + curr)/ arr.length)
+
+    //    console.log(result);
+    
+    // Rest Operator (Spread)
+
+        // let arr1 = [10, 20, 30]
+        // let arr2 = [1, 2, 3] 
+
+        // let result = [...arr1, ...arr2]
+
+        // console.log(result);
+
+// Objects: {key: value}
+
+    // let student1 = {name: "Siva", age: 23, dept: "IT"}
+
+    // console.log(student1);
+    // console.log(student1.name);
+    // console.log(student1.age);
+
+    // student1.age = 18
+    // student1.mobile = 987654321
+
+    // delete student1["dept"]
+
+    // console.log(student1);
+
+// Destructuring:
+
+    // 1. Array Destructuring
+
+        // let fruits = ["apple", "banana", "orange"]
+
+        // const [a, o, x] = fruits
+
+        // console.log(a);
+        // console.log(o);
+        // console.log(x);
+        
+    // 2. Object Destructuring
+
+        // let student1 = {name: "Siva", age: 23, dept: "IT"}
+
+        // let {name, dept, age} = student1
+
+        // console.log(name);
+        // console.log(dept);
+        // console.log(age);
+
+    // const data = [     --> JSON Format
+    //   {
+    //     name: "Siva",
+    //     age: 23,
+    //     dept: "IT",
+    //   },
+    //   {
+    //     name: "Jeeva",
+    //     age: 23,
+    //     dept: "IT",
+    //   },
+    //   {
+    //     name: "Kavitha",
+    //     age: 23,
+    //     dept: "IT",
+    //   },
+    //   {
+    //     name: "Kumar",
+    //     age: 23,
+    //     dept: "IT",
+    //   },
+    // ];
+    
+    
+
