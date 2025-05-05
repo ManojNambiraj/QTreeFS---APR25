8f54afc8359ca83ef17d18d924c79c2fbe8819d2
ManojNambiraj <postboxmano@gmail.com>
2025-05-05 19:33:20 +0530

    python class 1

---
+++ Python/class1/demo.py
@@ -0,0 +1,17 @@
+# Escape Sequence
+
+
+# Commands
+print('Hello, I"am fresher for \\Python programming')
+print("hi", end="\n")   
+print("Good evening", "Good morning", sep="/")
+
+# Variables:
+
+# a = 10
+# a = 10.5
+# a = "hello"
+# a = True
+# ageOne22 = 500
+
+# print(type(ageOne22))
