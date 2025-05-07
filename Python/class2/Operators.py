80b071fc49d79641d5cf886444eab00c1e07b470
ManojNambiraj <postboxmano@gmail.com>
2025-05-07 19:43:07 +0530

    Python Casting Operators & Precedance

---
+++ Python/class2/Operators.py
@@ -0,0 +1,51 @@
+# Strings:
+
+    # a = """
+    #     1. Arithmetic op
+    #     2. Assignment op
+    #     3. Comparison op
+    #     4. Logical op
+    #     5. Identity op
+    #     6. Membership op
+    # """
+
+    # print(a)
+    # print(len(a))
+
+# Operators:
+
+"""
+    1. Arithmetic op --> (+, -, *, /, **, %)
+    2. Assignment op --> (=, +=, -=, *=, /=, %=)
+    3. Comparison op --> (==, !=, >, <, >=, <=)
+    4. Logical op    --> (and, or, not)
+    5. Identity op   --> (is, is not)
+    6. Membership op --> (in, not in)
+"""
+
+# a = 10
+
+# # result = (a == 10) and (a >= 10)
+# result = (a != 10) or (a > 10)
+
+# print(not(result))
+
+# 5. Identity op   --> (is, is not)
+
+# x = ["apple", "banana", "mango", "pinaple", "grapes"]
+# y = ["apple", "banana", "mango", "pinaple", "grapes"]
+# z = x
+
+# print(x is z)
+
+# 6. Membership op --> (in, not in)
+
+# x = ["Apple", "banana", "mango", "pinaple", "grapes"]
+
+# print("apple" not in x)
+
+# Operator precedance
+
+    a = 100 * (5 + 3)
+
+    print(a)
---
+++ Python/class2/casting.py
@@ -0,0 +1,13 @@
+# Casting
+
+    # int
+    # float
+    # str
+
+        # a = "hello 1000 true"
+
+        # b = str(a)
+
+        # print(type(b))
+        # print(b)
+
