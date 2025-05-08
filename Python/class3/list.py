025c7b4caab7f1413459306e288b71ea9d23f5a9
ManojNambiraj <postboxmano@gmail.com>
2025-05-08 19:37:08 +0530

    Python list

---
+++ Python/class3/list.py
@@ -0,0 +1,75 @@
+# Datatypes:
+
+    # Text Type:	    str
+
+    # Numeric Types:	int, float
+
+    # Sequence Types:	list, tuple, range
+
+    # Mapping Type:	    dict
+
+    # Set Types:	    set, frozenset
+
+    # Boolean Type:	    bool
+
+    # Binary Types:	    bytes, bytearray, memoryview
+
+    # None Type:	    NoneType
+
+
+# List
+
+# fruits = ["apple", "banana", "orange", "grapes", "watermelon"]
+# #           0       1          2          3           4
+
+# print(fruits)
+    # print(type(fruits))
+    # print(fruits[1])    # Forward indexing
+    # print(len(fruits))
+
+# backward indexing
+
+    # print(fruits[-3])
+
+# Range of indexing
+
+    # print(fruits[1:4])
+    # print(fruits[-3:])  
+
+# Accessing
+
+    # fruits = ["apple", "banana", "orange", "grapes", "watermelon"]
+
+    # fruits[1:4] = ["dragon", "muskmelon"]
+
+# Adding Values 
+
+    # fruits.append("dragon")
+    # fruits.insert(2, "muskmelon")
+
+    # print(fruits)
+
+    # fruits2 = ["dragon", "muskmelon"]
+
+    # fruits.extend(fruits2)
+
+    # print(fruits)
+
+# Remove values form list
+
+    # fruits = ["apple", "orange", "grapes", "watermelon", "banana"]
+
+    # fruits.remove("orange")
+    # fruits.pop(2)
+    # fruits.clear()
+    # fruits.sort(reverse=True)
+
+# Joining Lists
+
+    # fruits = ["apple", "orange", "grapes", "watermelon", "banana"]
+    # vehicles = ["cars", "bikes"]
+
+    # result = fruits + vehicles
+
+
+    # print(result)
