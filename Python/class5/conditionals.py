c19dea1467e85ddd5aeb5cadd7138c073cb488ad
ManojNambiraj <postboxmano@gmail.com>
2025-05-13 20:00:25 +0530

    Control statements

---
+++ Python/class5/conditionals.py
@@ -0,0 +1,76 @@
+# Controll statements:
+
+    # 1. Contional Statements
+
+        # if
+
+            # age = 18
+
+            # if(age >= 18):
+            #     print("Eligible for voting")
+            #     if(age == 18):
+            #         print("It's equal")
+
+
+        # if else
+
+            # age = 16
+
+            # if(age >= 18):
+            #     print("Eligible for voting")
+            # else:
+            #     print("Not Eligible")
+
+        # elif
+
+            # age = 18
+
+            # if(age > 18):
+            #     print("Eligible for voting")
+            # elif(age == 18):
+            #     print("Partially allowed")
+            # else:
+            #     print("Not Eligible")
+
+
+    # 2. Looping Statements
+
+        # for loop
+
+        # Example 1:
+
+            # for i in range(0, 11, 3):
+            #     print(i)
+
+         # Example 2:
+
+            # fruits = ["apple", "banana", "grapes", "pineapple"]
+
+            # for item in fruits:
+            #     print(item)
+
+        # while loop
+
+            # a = 1
+
+            # while(a < 11):
+            #     print(a)
+            #     a = a + 1
+
+    # 3. Jumping Statements
+
+        # break
+
+            # for i in range(10):
+            #     if(i == 5):
+            #         break
+
+            #     print(i)
+
+        # continue
+
+            # for i in range(10):
+            #     if(i == 5):
+            #         continue
+
+            #     print(i)
