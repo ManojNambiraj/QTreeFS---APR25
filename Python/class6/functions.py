5bba8e33a9a87d583b96f7a278289815f7360723
ManojNambiraj <postboxmano@gmail.com>
2025-05-14 19:48:37 +0530

    Functions

---
+++ Python/class6/functions.py
@@ -0,0 +1,61 @@
+# Functions
+
+# Example 1:
+
+    # def demo():
+    #     print("Hi")
+
+    # demo()
+
+# Example 2:
+
+    # def demo(a, b):
+    #     print(a * b)
+
+    # demo(10, 5)
+
+# Example 3:
+
+    # def demo(a, b):
+    #     return a * b
+
+    # result = demo(7, 2)
+
+    # print(result)
+
+# Parameters (or) Arguments
+
+# Types of Arguments
+
+    # 1. Default Arguments
+
+        # def demo(a, b, c=1):
+        #     print(a, b, c)
+
+        # demo(2, 7, 10)
+
+    # 2. Positional Arguments
+
+        # def demo(a, b, c):
+        #     print(a, b, c)
+
+        # demo(2, 7, 10)
+
+    # 3. Keyword Arguments
+
+        # def demo(a, b, c):
+        #     print(a, b, c)
+
+        # demo(c = 2, a = "hello", b = 10)
+
+# lambda functions:
+
+    # Syntax:
+
+        # variable =  lambda arguments : expression
+
+    # Example:
+
+        # x = lambda a: a * 2
+
+        # print(x(5))
